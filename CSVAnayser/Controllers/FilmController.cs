f3f77d46acc1ecc3fd62402d81d3483b1aab4bd2
Supun Sethsara <sse@tiqri.com>
2017-12-12 14:43:34 +0530

    Add project files.

---
+++ CSVAnayser/Controllers/FilmController.cs
@@ -0,0 +1,73 @@
+﻿using CSVAnayser.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace CSVAnayser.Controllers
+{
+    public class FilmController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View(new List<FilmModel>());
+        }
+
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase postedFile)
+        {
+            List<FilmModel> film = new List<FilmModel>();
+            string filePath = string.Empty;
+            if (postedFile != null)
+            {
+                string path = Server.MapPath("~/Uploads/");
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+
+                filePath = path + Path.GetFileName(postedFile.FileName);
+                string extension = Path.GetExtension(postedFile.FileName);
+                postedFile.SaveAs(filePath);
+
+                //Read the contents of CSV file.
+                string csvData = System.IO.File.ReadAllText(filePath);
+
+
+                bool titles = true;
+              
+
+                //Execute a loop over the rows.
+                foreach (string row in csvData.Split('\n'))
+                {
+
+                    List<string> movieCatagories = new List<string>();
+                    if (!string.IsNullOrEmpty(row) && titles==false)
+                        {
+
+                        foreach (string catagory in (row.Split(',')[2]).Split('|'))
+                        {
+                            movieCatagories.Add(catagory);
+                        }
+
+
+                        film.Add(new FilmModel
+                        {
+                            movieId = Convert.ToInt32(row.Split(',')[0]),
+                            title = row.Split(',')[1],
+                            categories = movieCatagories
+
+
+                        });
+                    }
+                    titles = false;
+                }
+            }
+
+            return View(film);
+        }
+    }
+}
---
+++ CSVAnayser/Models/FilmModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CSVAnayser.Models
+{
+    public class FilmModel
+    {
+        
+           
+            public int movieId { get; set; }
+
+            
+            public string title { get; set; }
+
+            
+            public List<string> categories { get; set; }
+        
+    }
+}
